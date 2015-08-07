bd67a7ecf55bb3e762e296a329e18b11755beba2
diogo92 <diogo_p_ribeiro@hotmail.com>
2015-08-07 19:21:34 +0100

    Commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;
+
+	float lastPlayerX;
+	float nextPlayerX;
+	void Start(){
+		lastPlayerX = -100;
+	}
+	// Update is called once per frame
+	void Update () {
+		nextPlayerX = player.transform.position.x - 2.5f;
+
+		if (nextPlayerX > lastPlayerX) {
+			lastPlayerX=nextPlayerX;
+			transform.position=new Vector3(nextPlayerX,transform.position.y,transform.position.z);
+		}
+
+		Debug.Log (transform.position.x);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public float accelerationForce = 2f;
+	public float rotationForce = 1f;
+	void Update () {
+		
+		float rotation = Input.GetAxis("Horizontal");
+		float acceleration = Input.GetAxis("Vertical");
+		if(Mathf.Abs(GetComponent<Rigidbody2D>().angularVelocity)<150)
+		GetComponent<Rigidbody2D>().AddTorque(-rotation * rotationForce);
+		else {
+			if(GetComponent<Rigidbody2D>().angularVelocity>=150)
+				GetComponent<Rigidbody2D>().angularVelocity=100;
+			if(GetComponent<Rigidbody2D>().angularVelocity<=-150)
+				GetComponent<Rigidbody2D>().angularVelocity=-100;
+		}
+		GetComponent<Rigidbody2D>().AddRelativeForce(transform.right * Mathf.Abs(rotation) * accelerationForce);
+		if(rotation==0){
+			if(GetComponent<Rigidbody2D>().angularVelocity<0)
+				GetComponent<Rigidbody2D>().AddTorque(1f);
+			if(GetComponent<Rigidbody2D>().angularVelocity>0)
+				GetComponent<Rigidbody2D>().AddTorque(-1f);
+		}
+
+		Debug.Log (transform.forward);
+	}
+
+}
